486b0af546773c61b2dcb970757c96f7ef1416f8
M4RCK5 <user.andresmarcos@gmail.com>
2025-05-24 09:25:06 -0300

    Update Plutonium Black Ops.bat

--- Plutonium Black Ops.bat
+++ Plutonium Black Ops.bat
@@ -52,6 +52,7 @@ if %errorlevel% equ 3 (
 	echo 1-Set "/sp_minplayers" [4 Players Max].
 	echo 2-Start the match and wait for other players.
 	echo 3-Join using "/connect IP".
+	echo 4-Restart using "/fast_restart" if needed.
 )
 timeout /t 5
 
