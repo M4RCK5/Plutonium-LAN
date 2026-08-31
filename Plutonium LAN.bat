897a26c86150b8a8695680402d2ad25553017591
M4RCK5 <user.andresmarcos@gmail.com>
2026-08-31 07:13:29 -0300

    Update Plutonium LAN.bat

--- Plutonium LAN.bat
+++ Plutonium LAN.bat
@@ -3,7 +3,7 @@
 title Plutonium LAN
 
 set "workdir=%systemdrive%\Plutonium"
-findstr /i "portable" "%~n0" >nul 2>&1 && set "workdir=%~dp0Plutonium"
+echo %~n0 | findstr /i "portable" >nul 2>&1 && set "workdir=%~dp0Plutonium"
 taskkill /f /im "plutonium-launcher-win32.exe" 2>nul
 md "%workdir%" >nul 2>&1
 cd /d "%workdir%" 2>nul
